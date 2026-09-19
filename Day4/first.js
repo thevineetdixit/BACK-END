eda1116d5f7138fe9971787733d0062c0c0d0ca9
Vineet Dixit <vineetdixit727@gmail.com>
2026-09-19 16:15:41 +0530

    basics of http module

---
+++ Day4/first.js
@@ -0,0 +1,24 @@
+const http = require('http');
+//the http is a module in nodejs which allow us to create a server 
+
+const server = http.createServer((req,res)=>{
+    
+    //ab routing try krte h basics
+    //jo bhi domain or port ke aage ka link hota h wo req hoti h 
+    if(req.url==="/")
+    res.end("hello coder army");
+    else if(req.url==="/contacts")
+        res.end("these are my contacts");
+    else if(req.url === "/about")
+        res.end("this is my about page");
+    else 
+    res.end("han betichod");
+});//lo ye ban gya server
+
+//ab server ban gya ab wo sunega kaha,i mean where the server will lookout for request
+// server needs to be alert at a particular location for requests (a term called "LISTEN")
+
+//we can explicitely mark the location wehre this server http will listen 
+server.listen(4000, ()=>{
+    console.log("hello i am listening at port number 4000");
+});
