3f01e0c5abc5566facbff547788b5f92c59d6258
Vineet Dixit <vineetdixit727@gmail.com>
2026-09-25 16:51:42 +0530

    hh

--- Day08/first.js
+++ Day08/first.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+//what is middleware?,what will you answer
+// okay so you asked me what is middleware in backend ,middleware is all the code or functionality that comes in between our initial req and the final response,like we go throug multiple next function between our first app.use then to multiple middlewares that do specific tasks and then move forward they dont send response but they do participate in transferring data or can say manipulated data to the final response function
+//so that is called as middleware nin my terms i dont know what does google say about it lets listen to his bullshit 
+
+//In Express.js, middleware is a function that executes during the request-response lifecycle of an HTTP request. It sits "in the middle" between the raw incoming request from the client and the final route handler or response sent back to the client
+
 app.use("/user",(req,res) =>{ //this is our routing path and this is out function
     console.log("i am first");
     next();
